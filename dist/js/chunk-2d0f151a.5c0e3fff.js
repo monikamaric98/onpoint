fa12d9be081b8034732197ca73206afe238e0d3d
monikamaric98 <monika.maric@fpmoz.sum.ba>
2021-12-06 19:44:41 +0100

    first commit

---
+++ dist/js/chunk-2d0f151a.5c0e3fff.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f151a"],{"9fb9":function(e,t,n){"use strict";n.r(t);var i=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("Profili kozmetičkih i frizerskih salona:")])])}],r=n("2877"),s={},l=Object(r["a"])(s,i,a,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0f151a.5c0e3fff.js.map
---
+++ dist/js/chunk-2d21eab1.2daf47c0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21eab1"],{d719:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("Last chance termini sa najboljim cijenama :")])])}],s=t("2877"),i={},l=Object(s["a"])(i,a,c,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d21eab1.2daf47c0.js.map
