cf83d03d2a387b1cf050c32e7500a12a0f267c6c
monikamaric98 <monika.maric@fpmoz.sum.ba>
2021-12-06 20:29:34 +0100

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
+<<<<<<< HEAD
 
   ],
   baseUrl: "./projekti/pzi/2022/g8/"
+=======
+  ]
+
+>>>>>>> bee5b02d7210484a119c6ff781a0a5679a00f510
 
 }
